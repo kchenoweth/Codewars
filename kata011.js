8e8e139f94c6a979d1f430ec91b23a1bad76d3b6
Krysten Chenoweth <krystenchenoweth@gmail.com>
2022-06-03 10:02:11 -0500

    Daily Codewar

---
+++ kata011.js
@@ -0,0 +1,13 @@
+//Opposite Number - 8kyu
+
+//As simple as it gets, make whatever number is input the opposite, or negative.
+
+function opposite(number) {
+    return(-number)
+}
+
+//If you MUST do math, multiply the input by -1
+
+function opposite(number) {
+    return number * (-1);
+}
